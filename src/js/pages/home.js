c2eed449fdaac685081ac9a2b0366a3085b59968
Dapo-Obembe <obembedapo@gmail.com>
2025-06-14 10:39:19 +0100

    Removed defaults scripts added

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,29 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    /*=============== SWIPER JS ===============*/
-    let swiperPosts = new Swiper(".swiper-container", {
-        loop: false,
-        spaceBetween: 16,
-        grabCursor: true,
-
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        autoplay: {
-            delay: 5000,
-        },
-
-        breakpoints: {
-            700: {
-                slidesPerView: 1,
-            },
-            800: {
-                slidesPerView: 2,
-            },
-            1040: {
-                slidesPerView: 3,
-            },
-        },
-    });
+    // Add or initiate your script functions here.
 });
