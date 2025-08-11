078152122a1698e9a52c13c5ec9b0c5e83e3aafa
Dapo-Obembe <obembedapo@gmail.com>
2025-08-11 23:23:13 +0100

    Added the Hero Section Block Data

--- acf-blocks/hero-section/render.php
+++ acf-blocks/hero-section/render.php
@@ -25,6 +25,12 @@ if ( ! empty( $block['align'] ) ) {
 	$block_class .= ' align' . $block['align'];
 }
 
+// ACF DATA.
+$hero_section_title       = get_field( 'hero_section_title' );
+$hero_section_description = get_field( 'hero_section_description' );
+$hero_section_button      = get_field( 'hero_section_button' );
+$hero_section_image       = get_field( 'hero_section_image' );
+
 // Rendering in inserter.
 if ( isset( $block['data']['preview_home-page_help'] ) ) :
 	echo '<img src="' . $block['data']['preview_home-page_help'] . '" style="width:100%; height:auto;">';
@@ -35,8 +41,45 @@ else :
 
 <section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $block_class ); ?> min-h-screen bg-primary">
 	
-	<div class="container">
-		HERO SECTION
+	<div class="container flex flex-wrap lg:flex-nowrap justify-between gap-10">
+		<div class="basis-[100%] lg:basis-[50%]">
+			<?php if ( ! empty( $hero_section_title ) ) : ?>
+				<h1><?php echo esc_html( $hero_section_title ); ?></h1>
+				<?php else : ?>
+					<h1><?php echo esc_html__( 'This is the title for this section', 'positivus' ); ?></h1>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $hero_section_description ) ) : ?>
+				<p><?php echo esc_html( $hero_section_description ); ?></p>
+				<?php else : ?>
+					<p><?php echo esc_html__( 'Edit the description in the backend', 'positivus' ); ?></p>
+			<?php endif; ?>
+
+			<?php
+			if ( ! empty( $hero_section_button ) ) :
+				$button_title = $hero_section_button['title'];
+				$button_url   = $hero_section_button['url'];
+
+				echo awc_button( $button_title, $button_url, 'primary' ); // phpcs:ignore
+			endif;
+			?>
+
+		</div>
+
+		<?php
+		if ( ! empty( $hero_section_image ) ) :
+			echo wp_get_attachment_image(
+				$hero_section_image['ID'],
+				'large',
+				false,
+				array(
+					'class'   => 'basis-[100%] lg:basis-[50%] object-cover object-center',
+					'loading' => 'eager',
+				)
+			);
+			endif;
+		?>
+
 	</div>
 
 </section>
